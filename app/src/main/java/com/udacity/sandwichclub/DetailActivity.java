8094bf93fec1f3b6cb07fee8d6183da6171b7c7f
Tuan Le <tuan.le@altres.com>
2018-07-13 09:50:36 -1000

    Parsed sandwich json for details page

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,7 +3,9 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -43,7 +45,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +58,6 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
-
+    private void populateUI(Sandwich sandwich) {
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,52 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        JSONObject jsonObject;
+        try {
+            jsonObject = new JSONObject(json);
+
+            // Use gson to deserialize?
+            Sandwich sandwich = new Sandwich();
+
+            JSONObject nameObject = jsonObject.getJSONObject("name");
+            sandwich.setMainName(nameObject.getString("mainName"));
+            JSONArray akaJsonArray = nameObject.getJSONArray("alsoKnownAs");
+            ArrayList<String> akaList = new ArrayList<>();
+            for (int i = 0; i < akaJsonArray.length(); i++) {
+                akaList.add(akaJsonArray.getString(i));
+            }
+            sandwich.setAlsoKnownAs(akaList);
+
+            String placeOfOrigin = jsonObject.getString("placeOfOrigin");
+            sandwich.setPlaceOfOrigin(placeOfOrigin);
+
+            String description = jsonObject.getString("description");
+            sandwich.setDescription(description);
+
+            String image = jsonObject.getString("image");
+            sandwich.setImage(image);
+
+            JSONArray ingredientsJsonArray = jsonObject.getJSONArray("ingredients");
+            ArrayList<String> ingredients = new ArrayList<>();
+            for (int i = 0; i < ingredientsJsonArray.length(); i++) {
+                ingredients.add(ingredientsJsonArray.getString(i));
+            }
+
+            sandwich.setIngredients(ingredients);
+
+            return sandwich;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
